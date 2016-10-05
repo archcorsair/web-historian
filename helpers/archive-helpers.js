791947e92a0ca8afbf247897dc7ba98ba08f7197
archcorsair <archcorsair@gmail.com>
2016-10-05 16:04:37 -0700

    Implement readUrlsFromList to read from lists.txt

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -25,7 +25,15 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function() {
+exports.readListOfUrls = function(cb) {
+  fs.readFile(exports.paths.list, function (err, data) {
+    if (err) {
+      throw err;
+    } else {
+      var urls = data.toString().split('\n');
+      cb(urls);
+    }
+  });
 };
 
 exports.isUrlInList = function() {
