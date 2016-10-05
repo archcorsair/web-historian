55de7292f852a53a6e118e4a6ca5982e3b979074
archcorsair <archcorsair@gmail.com>
2016-10-05 15:56:37 -0700

    Add posted websites to sites.txt

--- web/request-handler.js
+++ web/request-handler.js
@@ -7,13 +7,27 @@ exports.handleRequest = function (request, response) {
   var url = request.url;
   var method = request.method;
 
-  if (url === '/') {
-    helper.serveAssets(response, archive.paths.siteAssets + '/index.html');
-  } else if (url === '/www.google.com') {
-    helper.serveAssets(response, archive.paths.archivedSites + url);
-  } else {
-    response.writeHead(404);
-    response.end();
+  if (method === 'GET') {
+    if (url === '/') {
+      helper.serveAssets(response, archive.paths.siteAssets + '/index.html');
+    } else if (url === '/www.google.com') {
+      helper.serveAssets(response, archive.paths.archivedSites + url);
+    } else {
+      response.writeHead(404);
+      response.end();
+    }
+  } else if (method === 'POST') {
+    var body = [];
+    request.on('data', function(chunk) {
+      body.push(chunk);
+    });
+    response.writeHead(302);
+    request.on('end', function() {
+      body = Buffer.concat(body).toString().slice(4);
+      console.log('body: ', body);
+      helper.updateAssets(body);
+      response.end();
+    });
   }
 
 };
