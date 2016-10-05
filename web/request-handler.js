9cc17960103e2b7e102ac3ebc8619e1ad6d7b52a
archcorsair <archcorsair@gmail.com>
2016-10-05 15:00:03 -0700

    Serve up archived website and handle 404s

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,5 +4,16 @@ var helper = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (request, response) {
-  helper.serveAssets(response, archive.paths.siteAssets + '/index.html');
+  var url = request.url;
+  var method = request.method;
+
+  if (url === '/') {
+    helper.serveAssets(response, archive.paths.siteAssets + '/index.html');
+  } else if (url === '/www.google.com') {
+    helper.serveAssets(response, archive.paths.archivedSites + url);
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
+
 };
